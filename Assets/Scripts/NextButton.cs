685e3226bee75341bd982cd4149b79f5ea64bac4
yuka10440 <yuka.t310@gmail.com>
2016-10-20 12:05:28 +0900

    ssi movie
    swipe to watch next movie

---
+++ Assets/Scripts/NextButton.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class NextButton : MonoBehaviour {
+
+    int scene_num;
+
+    void Start()
+    {
+        Scene scene = SceneManager.GetActiveScene();
+        scene_num = scene.buildIndex;
+    }
+
+    public void OnNextButtonClick()
+    {
+        SceneManager.LoadScene(scene_num + 1);
+
+        if (scene_num == 2)
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+
+}
